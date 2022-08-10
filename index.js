966267e4dbd8e1a4baabb528fb09f40bfd37f47f
xyushi <ayuship03@gmail.com>
2022-08-11 00:25:16 +0530

    done

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 class SortedList {
   constructor() {
-  
+        this.items=[];
+        this.length=0;
   }
-  add() {
-   
+  add(item) {
+         const copy_items=this.items;
+         copy_items.push(item)
+         copy_items.sort((a,b)=>a>b?1:-1);
+         this.items=copy_items;
+         this.length=this.items.length;
   }
-  get() {
-
-  }
+  get(position) {
+         if(position>this.length-1){
+           throw new Error('outOfBounds')
+         }
+         else{
+           return this.items[position];
+         }
+  } 
   max() {
-
+        if(this.length===0){
+          throw new Error('EmptySortedList');
+        }
+        else{
+          return Math.max(...this.items);
+        }
   }
   min() {
-
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    }
   }
   avg() {
-    
-   
+    if(this.length===0){
+      throw new Error('EmptySortedList')
+    }
+    return this.sum()/this.length
   }
-
   sum() {
-  
+      const sum=this.items.reduce((total,value)=>{
+        return total+value
+      },0)
+      return sum
   }
   
 };
-
+const object=new SortedList();
 module.exports = SortedList;
